d078e9bd93675d1e055a97df51c2ca209f034868
Matthew Cowie <matthew@cowie.me>
2016-01-21 16:03:34 -0600

    Allow specifying tables to restore

--- import_db.rb
+++ import_db.rb
@@ -24,9 +24,15 @@ OptionParser.new do |opts|
     puts VERSION
     exit
   end
+
+  opts.on("-t", "--tables [TABLES]", "Specify tables, comma separated") do |t|
+    options[:tables] = t.split(',')
+  end
 end.parse!
 
 options[:project] = ARGV[0]
+options[:import_type] = '--clean'
+
 abort "ERROR: You must specify a Heroku project" if options[:project].nil?
 
 options[:database] ||= "#{options[:project]}_development"
@@ -35,6 +41,10 @@ if options[:refresh]
   `heroku pg:backups capture -a #{options[:project]}`
 end
 
+if options[:tables]
+  options[:tables] = options[:tables].map { |t| "--table=#{t}" }.join(' ')
+end
+
 `curl -o latest.dump \`heroku pg:backups public-url -a #{options[:project]}\``
-`pg_restore --verbose --clean --no-acl --no-owner -h localhost -d #{options[:database]} latest.dump`
+`pg_restore --verbose #{options[:import_type]} --no-acl --no-owner -h localhost #{options[:tables]} -d #{options[:database]} latest.dump`
 `rm latest.dump`
